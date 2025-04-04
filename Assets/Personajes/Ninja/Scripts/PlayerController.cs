7653ef93a8f889d85aa2beffb72c4a662ad62cfa
Arturo UPN <arturo.upncaj@gmail.com>
2025-04-04 09:01:40 -0500

    primera clase

---
+++ Assets/Personajes/Ninja/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.linearVelocityX = 5;
+        }
+        rb.linearVelocityX = 0;
+    }
+}
