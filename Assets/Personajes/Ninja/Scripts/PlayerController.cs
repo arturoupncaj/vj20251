e78fe6c7b43a8dbe2b3e6a091912024f49722f03
Luigi Mendoza <ludk17@gmail.com>
2025-04-25 08:48:16 -0500

    add coliders

--- Assets/Personajes/Ninja/Scripts/PlayerController.cs
+++ Assets/Personajes/Ninja/Scripts/PlayerController.cs
@@ -5,21 +5,59 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rb;
     SpriteRenderer sr;
     Animator animator;
+
+    private bool puedeMoverseVerticalMente = false;
+    private float defaultGravityScale = 1f;
+    private bool puedeSaltar = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Debug.Log("Iniciando PlayerController");
+
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
+
+        defaultGravityScale = rb.gravityScale;
     }
 
     // Update is called once per frame
     void Update()
     {
         SetupMoverseHorizontal();
+        SetupMoverseVertical();
         SetupSalto();
-        
     }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemigo"))
+        {
+            ZombieController zombie = collision.gameObject.GetComponent<ZombieController>();
+            Debug.Log($"Colision con Enemigo: ${zombie.puntosVida}");
+            Destroy(collision.gameObject);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log($"Trigger con: {other.gameObject.name}");
+        if (other.gameObject.name == "Muro") {
+            puedeMoverseVerticalMente = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log($"Trigger con: {other.gameObject.name}");
+        if (other.gameObject.name == "Muro") {
+            puedeMoverseVerticalMente = false;
+            rb.gravityScale = defaultGravityScale;
+        }
+    }
+
 
     void SetupMoverseHorizontal() {
         rb.linearVelocityX = 0;
@@ -39,7 +77,23 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void SetupMoverseVertical() {
+        
+        if (!puedeMoverseVerticalMente) return;
+        rb.gravityScale = 0;
+        rb.linearVelocityY = 0;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.linearVelocityY = 10;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.linearVelocityY = -10;
+        }
+    }
+
     void SetupSalto() {
+        if (!puedeSaltar) return;
         if (Input.GetKeyUp(KeyCode.Space))
         {
             rb.linearVelocityY = 12.5f;
